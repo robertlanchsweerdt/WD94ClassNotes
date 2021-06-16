0dd28e11ce7c8d28f05d5bc192d68dd4987a2998
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-16 18:33:07 -0400

    initialize and completed Assignment Operator lesson for Gold Badge

---
+++ javaScriptJune2021/1-jsFundamentals/5-expressionsAndOperators/01-assignmentOperators.js
@@ -0,0 +1,34 @@
+// Assignment operator
+// lets you take a variable and initialize it some value
+
+let x = 20;
+
+// addition assignment operator
+// shorthand
+x += 1; //  x = x + 1
+console.log(x);
+
+// subtract assignment operator
+x -= 1; // x = x -1
+console.log(x);
+
+// multiplication assignment operator
+x *= 2; // x = x * 2
+console.log(x);
+
+// division assignment operator
+x /= 4; // x = x / 4;
+console.log(4);
+
+// remainder (modulus) assignment operator
+x %= 2; // x = x % 2
+console.log(x);
+
+// modulus operator
+console.log(5 % 3); // 5 / 3 -> 1 2/3 -> 2
+console.log(25 % 7); // 25 / 7 -> 3 4/7 -> 4
+
+// exponential assignment operator
+let y = 7;
+y **= 3; // 7 * 7 * 7
+console.log(y);
