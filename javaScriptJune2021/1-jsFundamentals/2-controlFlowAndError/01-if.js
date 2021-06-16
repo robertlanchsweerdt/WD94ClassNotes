c813dda770bc7b174714a86df3c06e8f90ea584c
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-16 18:51:17 -0400

    completed Gold Badge lesson on IF conditional

---
+++ javaScriptJune2021/1-jsFundamentals/2-controlFlowAndError/01-if.js
@@ -0,0 +1,23 @@
+// IF CONDIIONALS
+
+let isOn = true;
+
+// Example of EXPLICITLY checking against true value
+if (isOn === true) {
+  console.log('The light is on');
+}
+
+// Example of IMPLICITLY checking true value
+if (isOn) {
+  console.log('The light is on, yay!');
+}
+
+let weather = 65;
+if (weather < 70) {
+  console.log('Wear a jacket!');
+}
+
+let string = 'Tyler';
+if (string === 'Tyler' || false) {
+  console.log('if statements and conditionals are powerful!!');
+}
