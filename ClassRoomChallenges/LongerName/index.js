9d7453d71e8c64af698e04c91e8b015b47e458af
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-17 18:56:12 -0400

    completed the Bronze challenge

---
+++ ClassRoomChallenges/LongerName/index.js
@@ -0,0 +1,15 @@
+/* BRONZE
+​
+Write two variables. One will store your name and another will store a 
+friend's name. Find out what property you can use to check 
+how long the names are. Console log how many letters are in each name.
+​
+*/
+
+const myName = 'Robert';
+const friendName = 'Buddy';
+
+console.log(`${myName} is this many characters long: ${myName.length}`);
+console.log(
+  `${friendName} is this many characters long: ${friendName.length} `
+);
