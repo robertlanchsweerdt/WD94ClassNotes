9d9885e28247ac4ad0a5aacebba68ce6eb204c78
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-17 19:09:32 -0400

    completed Silver Challenge

--- ClassRoomChallenges/LongerName/index.js
+++ ClassRoomChallenges/LongerName/index.js
@@ -10,6 +10,31 @@ const myName = 'Robert';
 const friendName = 'Buddy';
 
 console.log(`${myName} is this many characters long: ${myName.length}`);
+
 console.log(
   `${friendName} is this many characters long: ${friendName.length} `
 );
+
+/* SILVER
+​
+Expand on what you have already done and write a conditional to see 
+who has the longer name. Using string interpolation console log who's name is longer. 
+Example Result: My name is longer than Adam's.
+​
+*/
+
+const myNameLength = myName.length;
+const friendNameLength = friendName.length;
+
+// IF-ELSE CONDITIONAL
+
+if (myName > friendName) {
+  console.log(`${myName}'s name is longer than ${friendName}'s`);
+} else {
+  console.log(`${friendName}'s name is longer than ${myName}'s`);
+}
+
+// TERNARY
+myNameLength > friendNameLength
+  ? console.log(`${myName}'s name is longer than ${friendName}'s`)
+  : console.log(`${friendName}'s name is longer than ${myName}'s`);
