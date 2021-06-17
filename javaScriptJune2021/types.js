7c1a6a799cb4432228b74c4eef2613229ef564d9
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-17 18:50:11 -0400

    completed lesson

---
+++ javaScriptJune2021/types.js
@@ -0,0 +1,21 @@
+/**
+ *  1. Booleans
+ *  2. Null
+ *  3. Undefined
+ *  4. Numbers -- maximum amount round to is 15, after 15 it rounds up
+ *  5. Strings
+ *  6. Objects
+ *  7. Arrays
+ *  8.
+ */
+
+const num = 9999999999999999;
+console.log(num);
+
+/**
+ *  Data type literals are fixed values that we as
+ *  developers insert into the code, not data that is
+ *  coming from a 3rd party source
+ */
+
+const name = 'teddy';
