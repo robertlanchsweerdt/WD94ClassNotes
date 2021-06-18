c10a64ceb713c0cb12270c955281913fff49fdc7
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-17 20:59:18 -0400

    completed Types Challenge

--- ClassRoomChallenges/Types/index.js
+++ ClassRoomChallenges/Types/index.js
@@ -2,13 +2,53 @@
 ​
 Create an Object that contains a string, number, boolean, and object.
 Console.log the type of one of the values in the object.
- 
-*/
-​
-​
+
+ */
+
+const myObj = {
+  myName: 'Robert',
+  age: 33,
+  elevenFiftyStudent: true,
+  someArr: [1, 2, 3, 4, 5],
+  address: {
+    street: '123 Main Street',
+    city: 'Anywhere',
+  },
+};
+
+const {
+  myName,
+  age,
+  elevenFiftyStudent,
+  someArr,
+  address,
+  address: { street, city },
+} = myObj;
+
+console.log(typeof myName);
+
 /* SILVER
 ​
 Write a conditional that checks the type of one of the values stored in the object 
 and console logs the data type. If the value is not a string, number, boolean, or object console log 'What are you?!'
 ​
 */
+
+const checkMe = foo;
+
+switch (typeof checkMe) {
+  case 'string':
+    console.log(` The typeof ${checkMe} is a string`);
+    break;
+  case 'number':
+    console.log(` The typeof ${checkMe} is a number`);
+    break;
+  case 'boolean':
+    console.log(` The typeof ${checkMe} is a boolean`);
+    break;
+  case 'object':
+    console.log(` The typeof ${checkMe} is an object`);
+    break;
+  default:
+    console.log(` The typeof ${checkMe} is unknown.  What are you?`);
+}
