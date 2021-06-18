202e53570bbd95cb41c7ffc04b5dc240f56d4138
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-17 20:14:46 -0400

    Initialize the Types Classroom Challenge

---
+++ ClassRoomChallenges/Types/index.js
@@ -0,0 +1,14 @@
+/* BRONZE
+​
+Create an Object that contains a string, number, boolean, and object.
+Console.log the type of one of the values in the object.
+ 
+*/
+​
+​
+/* SILVER
+​
+Write a conditional that checks the type of one of the values stored in the object 
+and console logs the data type. If the value is not a string, number, boolean, or object console log 'What are you?!'
+​
+*/
