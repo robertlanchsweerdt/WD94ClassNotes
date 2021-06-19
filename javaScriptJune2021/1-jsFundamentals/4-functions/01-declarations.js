80d50f767d19d0191fd3005cfab2dd0dd2ad7df1
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-19 11:07:47 -0400

    completed DELCARATIONS lesson

---
+++ javaScriptJune2021/1-jsFundamentals/4-functions/01-declarations.js
@@ -0,0 +1,13 @@
+/**
+ * FUNCTIONS
+ *
+ * Functions do:
+ *
+ * 1. take input (argument) which the function will handle/process;
+ * 2. they process the input given to them;
+ * 3. they usually, but not always, return some value;
+ * 4. they can be invoked (called) as many times as we want to reduce code repetition
+ *
+ * When a function doesn't return anything, it's called a 'SIDE EFFECT'
+ *
+ */
