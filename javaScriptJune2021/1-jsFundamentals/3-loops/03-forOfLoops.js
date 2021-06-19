62d6e1998b2ae0956106cb60b75c8410f461ade4
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-19 08:17:21 -0400

    update to FOR OF lesson

--- javaScriptJune2021/1-jsFundamentals/3-loops/03-forOfLoops.js
+++ javaScriptJune2021/1-jsFundamentals/3-loops/03-forOfLoops.js
@@ -1,7 +1,7 @@
 /**
  * FOR OF LOOPS
  *
- *  requires your object needs to be iterable --- that means it needs to be ordered / numbered;
+ *  requires your object needs to be *iterable* --- that means it needs to be ordered / numbered;
  *  Arrays are ordered / numbered so they work with FOR OF loops
  *  FOR OF returns the value where FOR IN returns the object key or array position number
  *
