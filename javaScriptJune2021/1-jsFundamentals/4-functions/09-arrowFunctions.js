d7de5d2386babed8be3e26eac9b498c0836755f2
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-19 11:18:10 -0400

    completed ARROW FUNCTIONS lesson

---
+++ javaScriptJune2021/1-jsFundamentals/4-functions/09-arrowFunctions.js
@@ -0,0 +1,26 @@
+/**
+ * ARROW FUNCTIONS
+ *
+ * Functional Declaration (hoisted)
+ * example:
+ * function newFunc(para){
+ *   // some code
+ * }
+ *
+ * Function Expression (not hoisted, referred to as anonymous function)
+ * example:
+ * let tea = function(){
+ *   // some code
+ * }
+ *
+ *
+ * Arrow Function (not hoisted, ES6, always anonymous function)
+ * example:
+ * let tea = () => {
+ *   // some code
+ * }
+ *
+ * With Arrow Functions, know BLOCK BODY (uses curly braces) v CONCISE BODY (NO curly braces or NO NEED to explicitly use RETURN keyword)
+ *
+ *
+ */
