6cf9126b3bbb95c582c31736f72c078c89d1d169
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-26 10:32:30 -0400

    instructor white board warm up challenge

---
+++ ClassRoomChallenges/WhiteBoarding/whiteBoardingWarmUp.js
@@ -0,0 +1,25 @@
+//Warm up white boarding challenges
+
+//Complete the challenge within a google doc
+
+// Set up two varibles equal to any two integers
+// Write a conditional that checks if the first varible is divisible by the second
+// If it is console log `value of ${a} is divisble by value of ${b}`
+// If it is not console log the remainder
+
+/**
+ * ANOTHER CHALLENGE
+ */
+
+//Checking a string
+
+// STEP 1
+//Write a conditional that checks if a string is empty
+//If it is console log 'Please type something'
+
+// STEP 2
+//If it is not console log the string
+
+// STEP 3
+//add to your conditional to check if the string has more than 10 characters
+//if it does console log please type less
